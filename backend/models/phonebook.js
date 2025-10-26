373d17b5e52b0fab9d5e16469de4cc6184eaadc6
divyesh-rathod <divyeshrathod096@gmail.com>
2025-10-25 20:25:15 -0400

    completed the final phase of the codeing

--- backend/models/phonebook.js
+++ backend/models/phonebook.js
@@ -22,7 +22,7 @@ const phoneSchema = new mongoose.Schema({
     required: [true, "User name required"],
   },
   number: {
-    type: Number,
+    type: String,
     validate: {
       validator: function (v) {
         return /\d{3}-\d{3}-\d{4}/.test(v);
